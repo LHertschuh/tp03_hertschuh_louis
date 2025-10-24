75f9454fa17c9aae5133d660a23bb07d969bcdf3
Louis Hertschuh <louishertschuh@MacBook-Air-de-Louis.local>
2025-10-24 11:27:44 +0200

    Render Front

--- Front/src/environments/environment.prod.ts
+++ Front/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
- // apiUrl: 'https://votre-api-render.onrender.com'
+  apiUrl: 'https://tp03-hertschuh-louis.onrender.com'
 };
